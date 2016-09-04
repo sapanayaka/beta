8e403a421690618d207631fa4b24424583f22cfa
Bernard Ribemont <bribemont@gmail.com>
2016-09-04 08:19:58 +0200

    Pristine

---
+++ config/puma.rb
@@ -0,0 +1,32 @@
+workers 2
+
+threads 1, 6
+
+app_dir = File.expand_path("../..", __FILE__)
+tmp_dir = "#{app_dir}/tmp"
+log_dir = "#{app_dir}/log"
+
+rails_env = ENV.fetch("RAILS_ENV") { "development" }
+environment rails_env
+
+bind "unix:///tmp/current.sock"
+
+daemonize
+
+stdout_redirect "#{log_dir}/puma.stdout.log", "#{log_dir}/puma.stderr.log", true
+
+pidfile "#{tmp_dir}/pids/puma.pid"
+state_path "#{tmp_dir}/pids/puma.state"
+
+# activate_control_app
+
+# preload_app!
+
+on_worker_boot do
+  require "active_record"
+  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+end
+
+# Allow puma to be restarted by `rails restart` command.
+plugin :tmp_restart
