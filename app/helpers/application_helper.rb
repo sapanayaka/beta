dfb9c7c4b096e283b110641667369e81c2b585b1
Bernard Ribemont <bribemont@gmail.com>
2016-09-04 18:06:36 +0200

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "(EXE)rcices en (L)angue (F)rançaise"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
